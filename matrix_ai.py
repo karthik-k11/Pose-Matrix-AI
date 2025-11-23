cc8ee6ac44cd9afa696568e80f4ea3c9344eb900
Karthik <karthikk1162@gmail.com>
2025-11-23 15:46:21 +0530

    Initialized mediapipe

--- matrix_ai.py
+++ matrix_ai.py
@@ -4,3 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import math 
+
+class MotionAnalyzer:
+    def __init__(self):
+        # Initialize MediaPipe Pose
+        self.mp_pose = mp.solutions.pose
+        self.pose_engine = self.mp_pose.Pose(
+            static_image_mode=False,
+            model_complexity=1, 
+            smooth_landmarks=True,
+            min_detection_confidence=0.6,
+            min_tracking_confidence=0.6
+        )
